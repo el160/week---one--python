82cc3c181b5c5915be4f922b72e845ef6098ce71
el160 <ljhongeri04@gmail.com>
2024-11-05 15:28:49 +0300

    Add user_input.py and week---one--python directory

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Default values
+
+
+name = input("What is your name? Elijah ") 
+
+age = input("How old are you? ",32) 
+
+location = input("Where do you live? Mombasa") 
+
+# Displaying a personalized message
+print(f"Hello, my name is {name}, I am {age} years old and live in {location}.")
